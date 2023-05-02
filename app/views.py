b6994e8e2a394543d28ac078cc09c25b1702ce12
mamathachamarthi <mamathachamarthi8@gmail.com>
2023-05-02 08:33:05 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def sample(request):
+    return render(request,'sample.html')
+
+
+def sample1(request):
+    return render(request,'sample1.html')
